48595944625363651cce41e43d5b19f903a4496b
GitHub <noreply@github.com>
2023-10-30 10:02:42 +0000

    Auto deploy from GitHub Actions build 6
    Palve: [6173800310] update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1319,8 +1319,8 @@ var store = [{
 "url": "/wiki/greenwarm/xen%E7%AE%80%E4%BB%8B%E4%B8%8E%E4%BD%BF%E7%94%A8/",
 "teaser": null
 },{
-"title": "中国科学技术大学 Linux 用户协会",
-"excerpt":"中国科学技术大学 Linux 用户协会（科大 LUG）是由在校的 GNU/Linux 爱好者发起并组成的全校性群众团体，是一个以推广自由软件文化、宣传自由软件价值为目的的协会组织。  了解更多   English Version   特色活动           LUG 每周小聚:       每周末由同学带来的技术分享演讲。            Linux Install Party:       推广和安装 Linux，宣传和鼓励开源和自由软件精神的活动。            Linux 101:       帮助新手学习 Linux 的系列活动。            自由软件日:       在国际自由软件基金会（SFI）的领导下，由自由/开源软件爱好者自发筹集的非盈利性活动。            信息安全大赛:       每年秋季学期举办的信息安全竞赛。       网络服务      开源软件镜像   LUG FTP(各种资源汇总)   网络启动服务   GitLab 代码托管   其他   请参阅 LUG 网络服务总则   联系我们      QQ 群: 适合于即时性较强的讨论，较正式的讨论还请使用邮件列表。具体群号请查看 qq.ustclug.org 的 TXT 记录。   邮件列表: ustc_lug (at) googlegroups.com 点击此处了解详情   IRC/Telegram 在线及时聊天，点击此处了解加入方式，平台互联由 TUNA 提供支持   瀚海星云 BBS: 瀚海星云 BBS Linux 版   官方邮箱:    活动室: 西区图书馆 206 室   更多联系方式请 戳这里   协会 LOGO      Designed by Shengyu Zhang on 2018-08-30   协会章程   LUG 的所有活动均依照协会章程开展。  ","categories": [],
+"title": "南阳理工学院-计算机与信息化处理协会",
+"excerpt":"南阳理工学院-计算机与信息化处理协会是由南阳理工学院在校的 GNU/Linux 爱好者发起并组成的团体，旨在搭建信息交流共享的平台，宣传自由软件的价值，提高自由软件社区文化氛围，推广自由软件的应用。 了解更多   English Version   特色活动           LUG 每周小聚:       每周末由同学带来的技术分享演讲。            Linux Install Party:       推广和安装 Linux，宣传和鼓励开源和自由软件精神的活动。            Linux 101:       帮助新手学习 Linux 的系列活动。            自由软件日:       在国际自由软件基金会（SFI）的领导下，由自由/开源软件爱好者自发筹集的非盈利性活动。            信息安全大赛:       每年秋季学期举办的信息安全竞赛。       网络服务      开源软件镜像   LUG FTP(各种资源汇总)   网络启动服务   GitLab 代码托管   其他   请参阅 LUG 网络服务总则   联系我们      QQ 群: 适合于即时性较强的讨论，较正式的讨论还请使用邮件列表。   邮件: ustc_lug (at) googlegroups.com 点击此处了解详情   官方邮箱:    活动室: 东南校区 13 教 211 室   更多联系方式请 戳这里   协会 LOGO      Designed by Shengyu Zhang on 2018-08-30   协会章程   LUG 的所有活动均依照协会章程开展。  ","categories": [],
 "tags": [],
 "url": "/wiki/",
 "teaser": null
