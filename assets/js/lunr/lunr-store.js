b6df0aa7d4faa8575d7449e488a0e20144e7bf12
GitHub <noreply@github.com>
2024-03-03 04:28:53 +0000

    Auto deploy from GitHub Actions build 25
    Palve: [55557e9cf5] Update mirrors.md

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -108,7 +108,7 @@ var store = [{
 "teaser": null
 },{
 "title": "南阳理工学院开源软件镜像",
-"excerpt":"mirror.nyist.edu.cn 由南阳理工学院、南阳理工学院信息化建设与管理中心管理，NYIST CIPS 维护。   机器域名   IPv4/v6: mirror.nyist.edu.cn  v4-only: mirror4.nyist.edu.cn  v6-only: mirror6.nyist.edu.cn   IPv4：222.139.215.198  IPv6：2001:da8:5018:8::80/64   支持的访问方式   http、https   rsync - 部分   提供的服务   请访问 mirrors 主页以获取完整列表。   使用帮助   请点击主页相关链接下的使用帮助，获取镜像使用帮助。     状态监控   在首页点击页面上方的同步状态链接，可以查看当前各镜像的同步状态，包括 成功与否/上游源/镜像体积等。 大家如果知道更好（更新、更快）的上游源，请联系我们。   如果在实际使用过程中发现镜像有问题，请联系我们。   联系方式   我们的邮箱： cips AT nyist.edu.cn  ","categories": [],
+"excerpt":"mirror.nyist.edu.cn 由南阳理工学院、南阳理工学院信息化建设与管理中心管理，NYIST CIPS 维护。   机器域名   IPv4/v6: mirror.nyist.edu.cn  v4-only: mirror4.nyist.edu.cn  v6-only: mirror6.nyist.edu.cn   教育网 IP：59.69.128.21 联通 IP：222.139.215.198 IPv6：2001:da8:5018:8::80   支持的访问方式   http、https   rsync - 部分   提供的服务   请访问 mirrors 主页以获取完整列表。   使用帮助   请点击主页相关链接下的使用帮助，获取镜像使用帮助。     状态监控   在首页点击页面上方的同步状态链接，可以查看当前各镜像的同步状态，包括 成功与否/上游源/镜像体积等。 大家如果知道更好（更新、更快）的上游源，请联系我们。   如果在实际使用过程中发现镜像有问题，请联系我们。   联系方式   我们的邮箱： cips AT nyist.edu.cn  ","categories": [],
 "tags": [],
 "url": "/wiki/server/mirrors/",
 "teaser": null
