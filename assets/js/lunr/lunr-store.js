4395e3468975bbf5430f63b06f76f4b8d8636cd4
GitHub <noreply@github.com>
2024-03-03 04:12:37 +0000

    Auto deploy from GitHub Actions build 20
    Palve: [cfe243afb0] Update index.md

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -54,7 +54,7 @@ var store = [{
 "teaser": null
 },{
 "title": "网络服务列表",
-"excerpt":"S 级服务      开源镜像站（HTTP 访问）：https://mirror.nyist.edu.cn     A 级服务      代码托管平台：       B 级服务      主页 (本 wiki)：&lt;&gt;            开源镜像站（定时同步；rsync 访问；帮助页面）：https://mirror.nyist.edu.cn         C 级服务               服务等级与服务标准           S 级：最优先保障服务可用性，24 小时故障监控，故障自动转移            A 级：优先保障服务可用性，24 小时故障监控，每日备份            B 级：关键数据备份            C 级：非保障类服务            未列入本列表的服务：实验性服务      ","categories": [],
+"excerpt":"S 级服务      开源镜像站（HTTP 访问）：https://mirror.nyist.edu.cn   A 级服务      代码托管平台（校内 访问）：https://git.nyist.edu.cn   B 级服务      主页 (本 wiki)：https://cips.nyist.edu.cn   网络启动：pxe.nyist.edu.cn   开源镜像站（定时同步；rsync 访问；帮助页面）：https://mirror.nyist.edu.cn   C 级服务      Grafana 监控 (校内 only)：https://grafana.nyist.edu.cn   服务等级与服务标准           S 级：最优先保障服务可用性，24 小时故障监控，故障自动转移            A 级：优先保障服务可用性，24 小时故障监控，每日备份            B 级：关键数据备份            C 级：非保障类服务            未列入本列表的服务：实验性服务      ","categories": [],
 "tags": [],
 "url": "/wiki/cips/services/",
 "teaser": null
